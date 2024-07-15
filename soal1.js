22eb480c7039a40e6c174a10c58b628a1bffed37
zahrohnk <zahrohkholifah12@gmail.com>
2024-07-15 11:00:27 +0700

    Menambahkan penjelasan kode

--- soal1.js
+++ soal1.js
@@ -1,20 +1,19 @@
+const prompt = require('prompt-sync')({sigint: true});
 function akarPangkat2(x) {
     if (x < 0) {
-        console.log("Tidak bisa input bilangan negatif");
+        console.log("Tidak bisa input bilangan negatif"); //Jika 'x' kurang dari nol maka akan mencetak isi console.log
     } else if (x % 2 !== 0) {
-        console.log("Tidak bisa input bilangan ganjil");
+        console.log("Tidak bisa input bilangan ganjil"); //Jika 'x' dibagi 2 tidak sama dengan nol ('x' bukan bil genap) maka akan mencetak isi console.log
     } else {
         const hasil = Math.sqrt(x);
-        console.log("Hasil akar pangkat 2 dari " + x + " adalah: " + hasil);
+        console.log("Hasil akar pangkat 2 dari " + x + " = " + hasil); //Jika 'x' bukan tidak memenuhi persyaratan diatasnya maka 'x' akan dihitung akarpangkat 2 dan dicetak hasilnya
     }
 }
-
-// Mengambil input dari pengguna
-const userInput = prompt("Masukkan bilangan genap:");
-const angka = parseFloat(userInput);
-
-if (isNaN(angka)) {
-    console.log("Input tidak valid. Harap masukkan angka.");
+//Input dari pengguna
+const userInput = prompt("Masukkan bilangan genap:"); //Inputan tersimpan di var 'userinput'
+const x = parseFloat(userInput); //Mengkonversi isi 'userinput' dari string ke desimal dan disimpan dalam var 'x'
+if (isNaN(x)) {
+    console.log("Input tidak valid. Harap masukkan angka."); //Jika 'x' NaN (Not a Number) atau bukan angka maka akan mencetak pesan input invalid
 } else {
-    akarPangkat2(angka);
+    akarPangkat2(x); //Jika 'x' angka maka akan mencetak hasil dari function x
 }
